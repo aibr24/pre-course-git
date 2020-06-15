9d857cc195864839e78a88fdaa56840a2b0ce1ac
aibr24 <aliibrahim4793@gmail.com>
2020-06-15 17:21:49 +0300

    added story.js task

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Ali";
+let age = 27;
+let hobby = "Flying";
+
+let story = `Mr./Mrs. ${name}, in 33 years, you will be ${
+  age + 33
+} years old. By then you'll have decades of ${hobby} practice!`;
+
+console.log(story);
